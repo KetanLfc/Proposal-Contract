141f6f2357371929a483826a5e6d85d56d11a565
KetanLfc <ketanlfc8@gmail.com>
2024-12-02 17:08:25 +0400

    Second Commit

--- contracts/ProposalContract.sol
+++ contracts/ProposalContract.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.18;
 
 contract ProposalContract {
     
+uint256 private counter; // Counter to keep track of proposal IDs
+
     struct Proposal {
         string title; // Title of the proposal for easier identification
         string description; // Description of the proposal
@@ -15,5 +17,20 @@ contract ProposalContract {
 }
 
 mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
+
+// Function to create a new proposal
+    function create(string calldata _title, string calldata _description, uint256 _total_vote_to_end) external {
+        counter += 1; // Increment the proposal ID counter
+        proposal_history[counter] = Proposal(
+            _title, // Set the title
+            _description, // Set the description
+            0, // Initialize approve votes to 0
+            0, // Initialize reject votes to 0
+            0, // Initialize pass votes to 0
+            _total_vote_to_end, // Set the vote limit
+            false, // Initialize current state as false (pending)
+            true // Mark the proposal as active
+        );
+    }
 
 }
